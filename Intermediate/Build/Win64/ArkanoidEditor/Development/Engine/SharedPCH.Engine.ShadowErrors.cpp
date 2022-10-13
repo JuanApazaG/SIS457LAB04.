c6c89f96a90a405830692bd21daba72ba7017b11
Agusti Apaza <juanagustin7887@gmail.com>
2022-10-13 07:51:38 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/ArkanoidEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/juana/OneDrive/Escritorio/SIS457/LABORATORIO_04/Arkanoid/Intermediate/Build/Win64/ArkanoidEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
